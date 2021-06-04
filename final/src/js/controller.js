c1d554b354b2e5cf8a60516155bd685ed7489719
nikosBatzonis <n.batzonis@gmail.com>
2021-06-04 22:44:13 +0200

    Added new welcome feature

--- final/src/js/controller.js
+++ final/src/js/controller.js
@@ -118,6 +118,9 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
+const newFeature = function () {
+  console.log('Welcome to the applications!');
+};
 
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -127,5 +130,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
